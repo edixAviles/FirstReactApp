cd998ebf5f2abb5c3d22892d75efb8c2c8e4f825
Edison Avilés <javier.aviles.munoz@gmail.com>
2019-05-02 11:43:23 -0500

    Inicial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const Container = () => (
+    <div className="jumbotron jumbotron-fluid">
+        <div className="container">
+            <Welcome />
+            <br/><br/>
+            <Switch />
+            <MyTable />
+        </div>
     </div>
-  );
-}
+);
 
-export default App;
+const Greetings = ({ nombre, apellido }) => <h1>Bienvenido {nombre} {apellido}...!</h1>;
+const Welcome = () => (
+    <div className="h1">
+        <Greetings nombre="Edison" apellido="Avilés" />
+    </div>
+    
+);
+
+const Switch = () => (
+    <div className="form-group">
+        <div className="custom-control custom-switch">
+            <input type="checkbox" className="custom-control-input" id="customSwitch1" />
+            <label className="custom-control-label" htmlFor="customSwitch1">Ejemplo de Switch</label>
+        </div>
+    </div>
+);
+
+const MyTable = () => (
+    <table className="table table-hover table-dark">
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Job</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Charlie</td>
+                <td>Janitor</td>
+            </tr>
+            <tr>
+                <td>Mac</td>
+                <td>Bouncer</td>
+            </tr>
+            <tr>
+                <td>Dee</td>
+                <td>Aspiring actress</td>
+            </tr>
+            <tr>
+                <td>Dennis</td>
+                <td>Bartender</td>
+            </tr>
+        </tbody>
+    </table>
+);
+
+export default Container
